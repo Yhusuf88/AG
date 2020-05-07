b9bde6d194cd2692676e180b23b83cc4b8672b23
Yhusuf88 <64873822+Yhusuf88@users.noreply.github.com>
2020-05-07 16:10:27 +0700

    Add files via upload

---
+++ 2GenetikdanValue.m
@@ -0,0 +1,13 @@
+clear
+clc
+
+target = 'Moh.Yusuf'
+
+panjang_target = length(target);
+
+gen_baru = create_gen(panjang_target)
+fitness = calculate_fitness(gen_baru,target)
+
+
+
+
---
+++ 3Populasi.m
@@ -0,0 +1,7 @@
+clear
+clc
+
+target = 'Moh.Yusuf || F55118068';
+
+besar_populasi = 5;
+populasi = create_population(target,besar_populasi);
